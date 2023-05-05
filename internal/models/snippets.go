387aad08ac65c17f65120190ca7b22406863bf1b
victorsmith <victor7smith@gmail.com>
2023-05-04 19:18:34 -0600

    added snippets object adn database config

---
+++ internal/models/snippets.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+// Wraps the connection pool
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// This will insert a new snippet into the database.
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) { 
+	return 0, nil 
+}
+
+// This will return a specific snippet based on its id.
+func (m *SnippetModel) Get(id int) (*Snippet, error) { 
+	return nil, nil 
+}
+
+// This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
+}
